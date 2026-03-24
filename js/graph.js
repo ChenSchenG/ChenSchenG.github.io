4a264fe780f0fc574fa9230780390a64e73fb923
ericscchen <ericscchen@tencent.com>
2026-03-24 20:13:18 +0800

    graph.js

--- js/graph.js
+++ js/graph.js
@@ -129,10 +129,10 @@
     });
 
     var sim = d3.forceSimulation(data.nodes)
-      .force('link', d3.forceLink(data.edges).id(function (d) { return d.id; }).distance(60))
-      .force('charge', d3.forceManyBody().strength(-120))
+      .force('link', d3.forceLink(data.edges).id(function (d) { return d.id; }).distance(75))
+      .force('charge', d3.forceManyBody().strength(-160))
       .force('center', d3.forceCenter(W / 2, H / 2))
-      .force('collision', d3.forceCollide().radius(14))
+      .force('collision', d3.forceCollide().radius(16))
       .force('x', d3.forceX(W / 2).strength(0.08))
       .force('y', d3.forceY(H / 2).strength(0.08));
 
